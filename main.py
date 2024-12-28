b3741fa941954685cd1c76efd6b3f296b98e9704
wojciechjurewicz <wojciechjurewicz2@gmail.com>
2024-12-28 19:38:51 +0100

    Added matrices with tettrominos shapes and their rotations

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import configparser
+from tetrominos import tetrominos as tetrominos_shapes
+
 def main():
-    pass
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    board_width = int(config['BoardSettings']['Width'])
+    board_height = int(config['BoardSettings']['Height']) + int(config['BoardSettings']['SafeZoneHeight'])
 
 if __name__ == '__main__':
     main()
